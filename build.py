c724551e62bf6007fdc0934cbe0c4df9b36d05ae
saihtaungkham <31495612+saihtaungkham@users.noreply.github.com>
2020-01-18 17:09:06 +0900

    Adding the functionalities to mlsearch package.

--- build.py
+++ build.py
@@ -1,17 +1,20 @@
 from pybuilder.core import use_plugin, init
 
 use_plugin("python.core")
-#use_plugin("python.unittest")
+use_plugin("python.unittest")
 use_plugin("python.install_dependencies")
 use_plugin("python.flake8")
-#use_plugin("python.coverage")
+use_plugin("python.coverage")
 use_plugin("python.distutils")
 
-requires_python = "==3.6.9"
-name = "search-api-requester"
+
+name = "mlsearch"
 default_task = ["install_dependencies", "publish"]
 
 
 @init
 def set_properties(project):
-    project.depends_on_requirements("requirements.txt")
+    project.set_property("coverage_break_build", False)
+    project.build_depends_on("mock")
+    project.build_depends_on("requests")
+    project.build_depends_on("pygithub")
